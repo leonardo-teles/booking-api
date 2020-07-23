9d522d300c99e87b2a53338f41fb7bbf0331fd56
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-07-23 02:37:04 -0300

    Consulta en Repositorio de Restaurantes

--- src/main/java/com/boot/repositories/RestaurantRepository.java
+++ src/main/java/com/boot/repositories/RestaurantRepository.java
@@ -1,5 +1,21 @@
 package com.boot.repositories;
 
-public class RestaurantRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.boot.entities.Restaurant;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+	Optional<Restaurant> findById(Long id);
+	
+	Optional<Restaurant> findByName(String name);
+	
+	@Query("SELECT r FROM Restaurant r")
+	public List<Restaurant> findRestaurants();
 }
