1a377313728e57c2a21331b99ea6331a4cdb8886
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-08-11 17:13:40 -0300

    Test del Metodo Buscar Restaurantes por el Id

--- src/test/java/com/boot/controller/RestaurantControllerTest.java
+++ src/test/java/com/boot/controller/RestaurantControllerTest.java
@@ -1,22 +1,44 @@
 package com.boot.controller;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.booking.controllers.RestaurantController;
 import com.booking.exceptions.BookingException;
+import com.booking.jsons.RestaurantRest;
+import com.booking.responses.BookingResponse;
 import com.booking.services.RestaurantService;
 
 public class RestaurantControllerTest {
 
+	private static final Long RESTAURANT_ID = 1L;
+	private static final String SUCCESS_STATUS = "Success";
+	private static final String SUCCESS_CODE = "200 OK";
+	private static final String OK = "OK";
+	
 	@Mock
 	RestaurantService restaurantService;
 	
 	@InjectMocks
 	RestaurantController restaurantController;
 	
+	@Before
 	public void init() throws BookingException {
 		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void getRestaurantByIdTest() throws BookingException {
+		final BookingResponse<RestaurantRest> response = restaurantController.getRestaurantById(RESTAURANT_ID);
+		assertEquals(response.getStatus(), SUCCESS_STATUS);
+		assertEquals(response.getCode(), SUCCESS_CODE);
+		assertEquals(response.getMessage(), OK);
+		//assertEquals(response.getData(), RESTAURANT_REST);
+		
 	}
 }
